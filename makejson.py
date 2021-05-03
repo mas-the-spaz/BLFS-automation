768503641798fda69bbf2c18fa4321477b6fcd52
mazz <aronmaslin@gmail.com>
2021-05-04 01:05:51 +0300

    real commit

--- makejson.py
+++ makejson.py
@@ -22,55 +22,86 @@ JSON scheme
 }
 '''
 
-
-def getInfo(i):
-        res = requests.get(i.rstrip(), verify=False)
-        soup = bs4(res.text, 'html.parser')
-        name = soup.find("h1", class_='sect1').find('a').get('id')  # string of name
-        version = soup.find("h1", class_='sect1').text.strip()  # string of version
-        deps = {'recommended': [], 'required': [], 'optional': []}
-        # if div class=sect2... in content,
-        # for each sect2 div, set all the vars etc. 
-
-        def depDict(type):
-            for i in soup.find_all('p', class_=type):
-                for j in i.find_all('a', title=True, class_='xref'): # grab blfs deps
-                   deps[type].append(j['title'])
-
-                for j in i.find_all('a', class_='ulink'): # greb external deps
-                    deps[type].append(j.text)
-                    scheme[j.text] = {'url': j['href']}
-                
-
-        for i in ['required', ' recommended', 'optional']:
-            depDict(i)
-
-        commands = []
-        for i in soup.find_all('kbd', class_='command'):  # remove whitespace
-            commands.append(re.sub('\s+\\\\\s+', ' ', i.text).replace('\n', ' '))
-
+def moduleCollect(soup):
+    # if div sect2 than call this
+    for module in soup.find_all('div', class_='sect2'):
+        ### if conatins text 'package information'
+        name = module.find('h2', class_='sect2').text.strip()
         urls = []
-        for i in soup.find('div', class_='itemizedlist').find_all('a', class_='ulink'):
+        for i in module.find('div', class_='itemizedlist').find_all('a', class_='ulink'):
             # if url starts with ftp... --- for dl script
             # check if returns something
             urls.append(i['href'])
 
+        deps = {'recommended': [], 'required': [], 'optional': []}
+        for y in ['required', 'recommended', 'optional']:   
+            for i in module.find_all('p', class_=y):
+                for j in i.find_all('a', title=True, class_='xref'): # grab blfs deps
+                    deps[y].append(j['title'])
+
+            for j in i.find_all('a', class_='ulink'): # greb external deps
+                deps[y].append(j.text)
+                scheme[j.text] = {'url': j['href']}
+        
+        commands = []
+        for i in module.find_all('kbd', class_='command'):  # remove whitespace
+            commands.append(re.sub('\s+\\\\\s+', ' ', i.text).replace('\n', ' '))
+
+        print(name)
+        print(deps)
         print(urls)
         print(commands)
-        print(name)
-        print(version)
-        print(deps)
+        scheme[name] = {'Version': name, 'url': urls, 'Dependencies': deps, 'Commands': commands}
 
-        scheme[name] = {'Version': version, 'url': urls, 'Dependencies': deps, 'Commands': commands}
+def packageCollect(soup):
+    name = soup.find("h1", class_='sect1').find('a').get('id')  # string of name
+    version = soup.find("h1", class_='sect1').text.strip()  # string of version
+    deps = {'recommended': [], 'required': [], 'optional': []}
+
+    def depDict(type):
+        for i in soup.find_all('p', class_=type):
+            for j in i.find_all('a', title=True, class_='xref'): # grab blfs deps
+                deps[type].append(j['title'])
+
+            for j in i.find_all('a', class_='ulink'): # greb external deps
+                deps[type].append(j.text)
+                scheme[j.text] = {'url': j['href']}
+            
+# get md5 ?
+    for i in ['required', 'recommended', 'optional']:
+        depDict(i)
+
+    commands = []
+    for i in soup.find_all('kbd', class_='command'):  # remove whitespace
+        commands.append(re.sub('\s+\\\\\s+', ' ', i.text).replace('\n', ' '))
+
+    urls = []
+    for i in soup.find('div', class_='itemizedlist').find_all('a', class_='ulink'):
+        # if url starts with ftp... --- for dl script
+        # check if returns something
+        urls.append(i['href'])
+
+    print(urls)
+    print(commands)
+    print(name)
+    print(version)
+    print(deps)
+
+    scheme[name] = {'Version': version, 'url': urls, 'Dependencies': deps, 'Commands': commands}
 
 
 
 with open('Wishlist.txt', 'r+') as u:  # eventually this will download the orignal urls
     for i in u.readlines():
         if not '#' in i:  # remove all modules urls 
-            getInfo(i)
+            res = requests.get(i.rstrip(), verify=False)
+            soup = bs4(res.text, 'html.parser') # get webpage contents
 
-
+            if soup.find_all('div', class_='sect2'):
+                moduleCollect(soup)
+            else:
+                packageCollect(soup)
+# consolidate into one function
 
 with open('dependencies.json', 'w+') as j:
     json.dump(scheme, j)
