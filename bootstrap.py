a5a43f2bd3f7c47dfb76f5ffa184e53f3164e4fd
mazz <aronmaslin@gmail.com>
2021-05-06 22:40:15 +0200

    hhh

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup as bs4
+from bs4 import BeautifulSoup as Bs4
 import json
 import re
 
@@ -30,52 +30,56 @@ links = []
 scheme = {}
 
 
+def StripText(string):
+    return re.sub(r'\n\s+', ' ', string)
+
+
 def packageCollect(package, tagClass, tag):
-    version = package.find(tag, class_=tagClass).text.strip()  # string of version
+    version = StripText(package.find(tag, class_=tagClass).text).strip()  # string of version
     name = package.find(tag, class_=tagClass).find('a').get('id')  # string of name
 
     deps = {'required': [], 'recommended': [], 'optional': []}
     for y in deps:
         for i in package.find_all('p', class_=y):
             for j in i.find_all('a', title=True, class_='xref'):  # grab blfs deps
-                deps[y].append(re.sub(r'\n\s+', ' ', j['title']))
+                deps[y].append(StripText(j['title']))
 
             for j in i.find_all('a', class_='ulink'):  # grab external deps
-                deps[y].append(re.sub(r'\n\s+', ' ', j.text))
-                scheme[re.sub(r'\n\s+', ' ', j.text)] = {'url': j['href']} # manually add url to scheme 
+                deps[y].append(StripText(j.text))
+                scheme[StripText(j.text)] = {'url': j['href']}  # manually add url to scheme
 
     commands = []
     for i in package.find_all('kbd', class_='command'):  # remove whitespace
-        commands.append(re.sub("\s+\\\\\\n\s+", ' ', i.text).replace('\n',' '))  # does not work with "r"
+        commands.append(re.sub("\s+\\\\\\n\s+", ' ', i.text).replace('\n', ' '))  # does not work with "r"
 
     urls = []
     if package.find('div', class_='itemizedlist'):
-        for i in package.find('div', class_='itemizedlist').find_all('a', class_='ulink'):  # if package has urls add to array
+        for i in package.find('div', class_='itemizedlist').find_all('a',
+                                                                     class_='ulink'):  # if package has urls add to array
             urls.append(i['href'])
 
     print("Downloading info for {0}".format(name))
     scheme[name] = {'Version': version, 'url': urls, 'Dependencies': deps, 'Commands': commands}
 
-res = requests.get(baseUrl, verify=False) # Begin...
-soup = bs4(res.text, 'html.parser')
-el = soup.find('a', attrs={"id":"package-index"}).parent.next_sibling.next_sibling
+
+res = requests.get(baseUrl, verify=False)  # Begin...
+soup = Bs4(res.text, 'html.parser')
+el = soup.find('a', attrs={"id": "package-index"}).parent.next_sibling.next_sibling
 print("Collecting base URLs....")
-for i in el.find_all('a', href=True): # for every url... check if has hashtag... if not add to array
+for i in el.find_all('a', href=True):  # for every url... check if has hashtag... if not add to array
     if not '#' in i['href']:
         links.append('https://www.linuxfromscratch.org/blfs/view/stable/' + i['href'])
-        
-        
 
 for i in links:
     res = requests.get(i.rstrip(), verify=False)
-    soup = bs4(res.text, 'html.parser')  # get webpage contents
+    soup = Bs4(res.text, 'html.parser')  # get webpage contents
 
     if soup.find_all('div', class_='sect2'):  # if soup is module instead of std package
         for module in soup.find_all('div', class_='sect2'):
             if module.find_all('div', class_='package'):  # limit to modules only
-                packageCollect(module, "sect2", "h2") # call function on module
+                packageCollect(module, "sect2", "h2")  # call function on module
     else:
-        packageCollect(soup, "sect1", "h1") # call function on std package
+        packageCollect(soup, "sect1", "h1")  # call function on std package
 
-with open('dependencies.json', 'w+') as j: # dump info to json file
+with open('dependencies.json', 'w+') as j:  # dump info to json file
     json.dump(scheme, j)
--- deps.py
+++ deps.py
@@ -27,9 +27,11 @@ check if ftp is only link - if yes download
 check if package is already downloaded
 check if package argument not exist
 
-if package only -- download function
-if -l package -- list function
-if -c or -o -- also do those deps
+if package only -- download function        ---pos
+if -l package -- list function              ---opt (default is download)
+if -r or -o -- also do those deps           ---opt (default is only required)
+if -a download all ONLY
+if -c list commands for that package
 
 
 '''
@@ -49,27 +51,47 @@ JSON scheme
         }
 }
 '''
-if not os.path.exists('dependencies.json'):
-    print(errors[0])
-    exit()
+
+
+def ListCommands(dat, arguments):
+    for command in dat[arguments.commands]['Commands']:  # ---list commands
+        print(command)
+
+
+def DownloadAll(dat):
+    for package in dat:
+        for url in dat[package]['url']:
+            print(url)
 
 
 def parserFunction(dat):
     parser = argparse.ArgumentParser(description='This script takes a valid BLFS package as an input and either lists '
                                                  'the dependencies, or downloads the necessary packages ', prog='deps')
-    parser.add_argument('-l', '--list', metavar='PACKAGE', help='List dependencies instead of downloading.')  # optional
-    parser.add_argument('-a', '--all', metavar='', help='Will download or list ALL packages. Uses a lot of time and '
-                                                        'space')  # optional
-    parser.add_argument('-c', '--commands', metavar='PACKAGE', help='List installation commands for a given package.')  # optional
-    parser.add_argument('-r', '--recommended', metavar='', help='Allow installation of recommended packages.')  # optional
-    parser.add_argument('-o', '--optional', metavar='', help='Allow installation of optional packages.')  # optional
-    parser.add_argument('package', help='A BLFS package taken from the book', type=str)  # pos
+    parser.add_argument('-a', '--all', help='Will download ALL packages. Uses a lot of time and '
+                                            'space', action='store_true')
+    parser.add_argument('-c', '--commands', metavar='PACKAGE', help='List installation commands for a given package.',
+                        default=False)
+    parser.add_argument('-l', '--list', metavar='PACKAGE', help='List dependencies instead of downloading.',
+                        default=False)
+    parser.add_argument('-o', '--optional', metavar='', help='Allow installation of optional packages.', default=False)
+    parser.add_argument('-r', '--recommended', metavar='', help='Allow installation of recommended packages.',
+                        default=False)
+    parser.add_argument('-d', '--download', help='Download given BLFS package', metavar='PACKAGE')
     args = parser.parse_args()
-    if args.package in dat:
-        # print(data[args.package])
-        for command in dat[args.package]['Commands']:  # ---list commands
-            print(command)
+    # if no args print help
 
+    if args.download in dat:
+        # case break
+        if args.commands:
+            ListCommands(dat, args)
+
+    if args.all:
+        DownloadAll(dat)
+
+
+if not os.path.exists('dependencies.json'):
+    print(errors[0])
+    exit()
 
 with open('dependencies.json', 'r+') as scheme:
     data = json.load(scheme)
