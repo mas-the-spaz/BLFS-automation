876272b1e1d91d9368697c8f1e28b871d90b7f35
mazz <aronmaslin@gmail.com>
2021-05-06 15:11:09 +0200

    zzzzzzzzzzzzzz

--- deps.py
+++ deps.py
@@ -3,14 +3,15 @@ import argparse
 import json
 import os
 
-errors = ["Dependencies.json not found! Try running bootstrap.py to rebuild dependency database", "no dependencies found for "]
+errors = ["Dependencies.json not found! Try running bootstrap.py to rebuild dependency database",
+          "no dependencies found for (package arguments)"]
 
 '''
 flags 
     list or DOWNLOAD?
     -l --list
 
-    include recommneded and optional?
+    include recommended and optional?
     -r --recommended, -o --optional
 
     list commands (possibly output to shell) or NORMAL (download)
@@ -25,15 +26,41 @@ main function
 output specific order of installation 
 check if ftp is only link - if yes download
 check if package is already downloaded
-check if inputed package not exist
+check if package argument not exist
 
 '''
-if not os.path.exists('dependencies.json'):
-    print(errors[0])
-    exit
+#if not os.path.exists('dependencies.json'):
+ #   print(errors[0])
+  #  exit
+
+
+    
+
+
+def parserFunction(data):
+    parser = argparse.ArgumentParser(description='''This script takes a valid BLFS package as an 
+                                                    input and either lists the dependencies,
+                                                    or downloads the necessary packages ''')
+    parser.add_argument('-l', '--list', help='List dependencies instead of downloading.')
+    parser.add_argument('-c', '--commands', help='List installation commands for a given package.')
+    parser.add_argument('-r', '--recommended', help='Allow installation of recommended packages.')
+    parser.add_argument('-o', '--optional', help='Allow installation of optional packages.')
+    parser.add_argument('-p', '--package', help='A BLFS package taken from the book', required=True, type=str)
+    args = parser.parse_args()
+    if args.package in data:
+        print(data[args.package])
+
 
 with open('dependencies.json', 'r+') as scheme:
     data = json.load(scheme)
-    
-print(data[str(sys.argv[1])])
+
+parserFunction(data)
+
+
+
+
+
+
+
+
 
