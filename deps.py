31c2294cd4f1b8fc0b9862d30be5f16b5ed3aed2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 16:16:34 +0000

    Adding tarfile member sanitization to extractall()

--- deps.py
+++ deps.py
@@ -153,7 +153,26 @@ class Actions(object):
                 file_to_extract = self.database[pkg]['url'][0]
                 if tarfile.is_tarfile(os.path.basename(file_to_extract)):
                     with tarfile.open(os.path.basename(file_to_extract), 'r') as tar_ref:
-                        tar_ref.extractall()
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar_ref)
                         os.chdir(tar_ref.getnames()[0].split('/', 1)[0])
 
                 if zipfile.is_zipfile(os.path.basename(file_to_extract)):
